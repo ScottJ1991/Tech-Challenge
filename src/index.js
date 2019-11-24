2fdee654d808c41e277de6bb411f8b03aeaef70d
Scott Jones <scottjonesuk1991@gmail.com>
2019-11-24 16:30:44 +0000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 //Import libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import Report1 from './components/Report1';
-import Report2 from './components/Report2';
-import Report3 from './components/Report3';
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Report1 from "./components/Report1";
+import Report2 from "./components/Report2";
+import Report3 from "./components/Report3";
 
 //Component is a 'Function'(no state) or 'Class'
 //const App = function(){
 const App = () => {
-    return (
-        <BrowserRouter>
-            <div>
-                <NavBar/>
-                <Route exact path='/' component={Home}/>
-                <Route path='/R1' component={Report1}/>
-                <Route path='/R2' component={Report2}/>
-                <Route path='/R3' component={Report3}/>
-            </div>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route path="/R1" component={Report1} />
+        <Route path="/R2" component={Report2} />
+        <Route path="/R3" component={Report3} />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
-    <App />,
-    //root in public/index.html
-    document.querySelector('#root')
+  <App />,
+  //root in public/index.html
+  document.querySelector("#root")
 );
