23824b4b5fb322cd60583933f5eef1695ed6ba82
Scott Jones <scottjonesuk1991@gmail.com>
2019-11-25 13:54:51 +0000

    Add files via upload

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const List = (props) => {
-    //console.log({props});
-    const Organiser = props.Event.organiser;
-    console.log(Organiser);
-    return(
-        <Row>
-            <Col sm={2}>Envent ID: {props.Event.id}</Col>
-            <Col sm={3}>Event tilte: {props.Event.title}</Col>
-            <Col sm={4}>Event description: {props.Event.description}</Col>
-            <Col sm={3}>Organiser name: </Col>
-        </Row>
-    )
-}
+const List = props => {
+  //console.log({props});
+  //const Organiser = props.Event.organiser;
+  //console.log(Organiser);
+  //console.log(props.Event.organiser.forename);
+  return (
+    <Row>
+      <Col sm={2}>Event ID: {props.Event.id}</Col>
+      <Col sm={3}>Event tilte: {props.Event.title}</Col>
+      <Col sm={4}>Event description: {props.Event.description}</Col>
+      <Col sm={3}>
+        Organiser name: {props.Event.organiser.forename} {props.Event.organiser.surname}
+      </Col>
+    </Row>
+  );
+};
 
 export default List;
