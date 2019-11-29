2c3dcdc529b00a9e1f7f19fd8f794b6e22a7e195
Scott Jones <scottjonesuk1991@gmail.com>
2019-11-29 13:15:22 +0000

    Add files via upload

--- src/components/staticVar.js
+++ src/components/staticVar.js
@@ -1,1 +1,2 @@
-export const baseUrl = "http://localhost:8080";
+//export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://2pstg7dl04.execute-api.eu-west-1.amazonaws.com/dev";
